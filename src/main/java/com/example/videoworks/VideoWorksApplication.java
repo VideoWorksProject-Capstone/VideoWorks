3bce2da5c82d4ab9a63ba81739a759e208ea7131
Jordan Foote <82241562+jordanfoote@users.noreply.github.com>
2022-06-06 10:13:28 -0500

    First Commit

---
+++ src/main/java/com/example/videoworks/VideoWorksApplication.java
@@ -0,0 +1,13 @@
+package com.example.videoworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoWorksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoWorksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/videoworks/VideoWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videoworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoWorksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
