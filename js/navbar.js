5177e4211d05ceaf837e4c2a436aa37cc95ecff9
Richard Jackson <rpjackson2@gmail.com>
2022-06-06 10:48:13 -0500

    created navbar

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+// For mobile/hamburger menu
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+    overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+    overlay.classList.remove("overlay--active");
+});
