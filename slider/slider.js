c36d36b3a673a3f1e1537a715674a9bbe029d519
Richard Jackson <rpjackson2@gmail.com>
2022-06-12 22:05:57 -0500

    halfway done with the hero section and graphics

--- slider/slider.js
+++ slider/slider.js
@@ -2,12 +2,31 @@
 
 let avatarHoverBox = document.querySelector(".avatar-hover-box");
 let avatarBox = document.querySelector(".media-element .user-avatar-box");
+let userAvatarBoxImg = document.querySelector(".media-element .user-avatar-box .user-avatar-image1");
+// let mediaElement = Array.from(document.querySelectorAll(".media-element"));
 
-avatarHoverBox.addEventListener("mouseover",(e)=>{
-    avatarBox.classList.add("hovered");
-    console.log("eeennnttteerrr")
-})
+let body = document.querySelector("body");
+let hamburgerMenuIcon = document.querySelector(".ham");
+hamburgerMenuIcon.addEventListener("click",()=> body.classList.toggle("no-scroll"));
 
-avatarHoverBox.addEventListener("mouseout",(e)=>{
-    avatarBox.classList.remove("hovered");
-})
+
+// mediaElement.forEach((element)=>{
+//     element.addEventListener("mouseover",(e)=>{
+//         avatarBox.classList.add("hovered");
+//         userAvatarBoxImg.classList.add("hovered");
+//         console.log("eeennnttteerrr")
+//     })
+// })
+
+
+
+// mediaElement.addEventListener("mouseover",(e)=>{
+//     avatarBox.classList.add("hovered");
+//     userAvatarBoxImg.classList.add("hovered");
+//     console.log("eeennnttteerrr")
+// })
+
+// mediaElement.addEventListener("mouseout",(e)=>{
+//     avatarBox.classList.remove("hovered");
+//     userAvatarBoxImg.classList.remove("hovered");
+// })
