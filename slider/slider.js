410e9690472045d4c3b6c998cd4bf65a343f2a23
Richard Jackson <rpjackson2@gmail.com>
2022-06-15 14:25:01 -0500

    added js to slider

--- slider/slider.js
+++ slider/slider.js
@@ -1,32 +1,28 @@
 "use strict"
 
 let avatarHoverBox = document.querySelector(".avatar-hover-box");
-let avatarBox = document.querySelector(".media-element .user-avatar-box");
+let avatarBox = Array.from(document.querySelectorAll(".media-element .user-avatar-box"));
 let userAvatarBoxImg = document.querySelector(".media-element .user-avatar-box .user-avatar-image1");
+let sliderCards = Array.from(document.querySelectorAll(".media-element"));
+let colorsArray = ["--green", "--blue", "--pink", "--purple", "--orange"];
+let mediaElementBorder = Array.from(document.querySelectorAll(".media-element__border"));
+
 // let mediaElement = Array.from(document.querySelectorAll(".media-element"));
+
+changeAvatarColors(avatarBox, mediaElementBorder);
+
 
 let body = document.querySelector("body");
 let hamburgerMenuIcon = document.querySelector(".ham");
-hamburgerMenuIcon.addEventListener("click",()=> body.classList.toggle("no-scroll"));
+hamburgerMenuIcon.addEventListener("click", () => body.classList.toggle("no-scroll"));
 
 
-// mediaElement.forEach((element)=>{
-//     element.addEventListener("mouseover",(e)=>{
-//         avatarBox.classList.add("hovered");
-//         userAvatarBoxImg.classList.add("hovered");
-//         console.log("eeennnttteerrr")
-//     })
-// })
-
-
-
-// mediaElement.addEventListener("mouseover",(e)=>{
-//     avatarBox.classList.add("hovered");
-//     userAvatarBoxImg.classList.add("hovered");
-//     console.log("eeennnttteerrr")
-// })
-
-// mediaElement.addEventListener("mouseout",(e)=>{
-//     avatarBox.classList.remove("hovered");
-//     userAvatarBoxImg.classList.remove("hovered");
-// })
+function changeAvatarColors(avatarBox, mediaElementBorder){
+    for(let i = 0; i < sliderCards.length; i++){
+        let rand = Math.floor(Math.random() * colorsArray.length);
+        sliderCards[i].style.borderTop = `var(${colorsArray[rand]}) solid 5px`;
+        sliderCards[i].style.borderBottom = `var(${colorsArray[rand]}) solid 5px`;
+        avatarBox[i].style.backgroundColor = `var(${colorsArray[rand]})`;
+        mediaElementBorder[i].style.borderTop = `var(${colorsArray[rand]}) dashed 4px`;
+    }
+}
