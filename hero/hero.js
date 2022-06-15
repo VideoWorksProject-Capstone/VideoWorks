0975a3e4698466adc893b963abed5113b31ab9c3
Richard Jackson <rpjackson2@gmail.com>
2022-06-14 23:11:31 -0500

    finished homepage

--- hero/hero.js
+++ hero/hero.js
@@ -1,10 +1,10 @@
 "use strict";
 
-let heroLedeBtn = document.querySelector(".hero-lede-btn");
-let heroLedeBtnArrow = document.querySelector(".fa-arrow-right");
-
-
-let growHeroLedeBtnFunc = () =>{
-    heroLedeBtn.classList.toggle()
-}
-heroLedeBtn.addEventListener("click",growHeroLedeBtnFunc)
+// let heroLedeBtn = document.querySelector(".hero-lede-btn");
+// let heroLedeBtnArrow = document.querySelector(".fa-arrow-right");
+//
+//
+// let growHeroLedeBtnFunc = () =>{
+//     heroLedeBtn.classList.toggle()
+// }
+// heroLedeBtn.addEventListener("click",growHeroLedeBtnFunc)
