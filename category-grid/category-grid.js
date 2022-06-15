a9a51ae76aad8805969c6cd94e6085a31b84bc46
Richard Jackson <rpjackson2@gmail.com>
2022-06-14 22:41:24 -0500

    refactoring homepage code

--- category-grid/category-grid.js
+++ category-grid/category-grid.js
@@ -1,39 +1,66 @@
-"use strict"
-
-let categoryCardsPopupPerson = document.querySelector(".category-cards__person-popup");
-let categoryCards = document.querySelector(".category-cards");
-let categoryShape = document.querySelector(".category-cards__shape");
-let categoryImg = document.querySelector(".category-cards__img");
-let categoryImgBlur = document.querySelector(".category-cards:hover .category-cards__img");
-let isMouseHover = false;
-// let cardDomRect = categoryCards.getBoundingClientRect();
-
-
-
-
-
-categoryCards.addEventListener("mouseover",()=> {
-
-
-        setTimeout(() => mouseOutFunc(), 2000);
-        categoryCardsPopupPerson.classList.add("hovered");
-        categoryShape.classList.add("hovered");
-        categoryImg.classList.add("hovered");
-        // categoryImgBlur.classList.add("hovered");
-        isMouseHover = true;
-        console.log(categoryCards.getBoundingClientRect())
-
-}, {capture:true, once:true});
-
-
-let mouseOutFunc = ()=> {
-    categoryCardsPopupPerson.classList.remove("hovered");
-    categoryShape.classList.remove("hovered");
-    categoryImg.classList.remove("hovered");
-    // categoryImgBlur.classList.remove("hovered");
-    isMouseHover
-}
-
-
-// Event Listener
-categoryCards.addEventListener("mouseout",mouseOutFunc);
+// "use strict"
+//
+// let categoryCardsPopupPerson = document.querySelector(".category-cards__person-popup");
+// // let categoryCards = Array.from(document.querySelectorAll(".category-cards"));
+// let categoryCards = document.querySelectorAll(".category-cards");
+// let listOfCards = [...categoryCards];
+// console.log(listOfCards)
+// let categoryShape = document.querySelector(".category-cards__shape");
+// let categoryImg = document.querySelector(".category-cards__img");
+// let categoryImgBlur = document.querySelector(".category-cards:hover .category-cards__img");
+// let isMouseHover = false;
+// // let cardDomRect = categoryCards.getBoundingClientRect();
+//
+// console.log(categoryCards.length)
+//
+// let mouseOutFunc = ()=> {
+//     categoryCardsPopupPerson.classList.remove("hovered");
+//     categoryShape.classList.remove("hovered");
+//     categoryImg.classList.remove("hovered");
+//     // categoryImgBlur.classList.remove("hovered");
+//     // isMouseHover =false;
+// }
+//
+//
+//
+// // let listOfCards = [...categoryCards];
+//
+// for (let card of listOfCards){
+//     card.addEventListener("click",()=> {
+//         // setTimeout(() => mouseOutFunc(), 2000);
+//         categoryCardsPopupPerson.classList.add("hovered");
+//         categoryShape.classList.add("hovered");
+//         categoryImg.classList.add("hovered");
+//         // categoryImgBlur.classList.add("hovered");
+//         // isMouseHover = true;
+//         // console.log(`${card} and ${index}`)
+//     });
+//
+//     card.addEventListener("mouseout",mouseOutFunc);
+// }
+// // categoryCards.forEach((card,index) =>{
+// //     card.addEventListener("click",()=> {
+// //         // setTimeout(() => mouseOutFunc(), 2000);
+// //         categoryCardsPopupPerson.classList.add("hovered");
+// //         categoryShape.classList.add("hovered");
+// //         categoryImg.classList.add("hovered");
+// //         // categoryImgBlur.classList.add("hovered");
+// //         // isMouseHover = true;
+// //         console.log(`${card} and ${index}`)
+// //
+// // });
+//
+//
+//
+//
+// // })
+//
+// // console.log(listOfCards)
+//
+//
+//
+//
+//
+//
+//
+// // Event Listener
