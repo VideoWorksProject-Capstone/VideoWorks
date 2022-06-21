5de7b922f39e2a5b923e73e7d6d6afa89b14f1a4
desktop-o0gpqr4\antsh <ants.hernandez@gmail.com>
2022-06-21 09:52:20 -0500

    updating work

--- profile/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-$(() => {
-    $('#edit-preferences').click(function(){
-        $('#edit-preferences-modal').addClass('is-active');
-    });
-    $('.modal-card-head button.delete, .modal-save, .modal-cancel').click(function(){
-        $('#edit-preferences-modal').removeClass('is-active');
-    });
-});
